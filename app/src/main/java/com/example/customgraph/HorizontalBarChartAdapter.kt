dc592d5e3055b3e383b325f687e16a1f1e550a01
XamDroidOfficial <xamdroidofficial@gmail.com>
2022-01-22 20:53:34 +0530

    Added horizontal bar chart

---
+++ app/src/main/java/com/example/customgraph/HorizontalBarChartAdapter.kt
@@ -0,0 +1,63 @@
+package com.example.customgraph
+
+import android.util.TypedValue
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.customgraph.databinding.ViewholderItemHorizontalBarChartBinding
+import kotlin.math.roundToInt
+
+class HorizontalBarChartAdapter(
+    private var pointList: List<Int>,
+    private var maxValueOnXAxis: Int
+) :
+    RecyclerView.Adapter<HorizontalBarChartAdapter.HorizontalBarChartViewHolder>() {
+
+    private var parentWidth: Int = 0
+    private var viewWidthInPx: Int = 0
+    private var marginInDp: Float = 16f
+    private var marginInPx: Float = 0f
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): HorizontalBarChartViewHolder {
+        val binding = ViewholderItemHorizontalBarChartBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+
+        marginInPx = convertMarginInPxFromDp(parent)
+        parentWidth = getParentWidthInPx(parent)
+
+        return HorizontalBarChartViewHolder(binding)
+    }
+
+    private fun convertMarginInPxFromDp(parent: ViewGroup) = TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        marginInDp,
+        parent.context.resources.displayMetrics
+    )
+
+    private fun getParentWidthInPx(parent: ViewGroup) =
+        (parent.context.resources.displayMetrics.widthPixels - (marginInPx * 2)).roundToInt()
+
+    // Below formula is similar as we use for calculating percentage.
+    private fun calculateViewWidth(value: Int): Int {
+        return value * parentWidth / maxValueOnXAxis
+    }
+
+    override fun onBindViewHolder(holder: HorizontalBarChartViewHolder, position: Int) {
+        viewWidthInPx = calculateViewWidth(pointList[position])
+        holder.binding.view.layoutParams.width = viewWidthInPx
+        holder.binding.textValue.text = pointList[position].toString()
+    }
+
+    override fun getItemCount(): Int {
+        return pointList.size
+    }
+
+    inner class HorizontalBarChartViewHolder(val binding: ViewholderItemHorizontalBarChartBinding) :
+        RecyclerView.ViewHolder(binding.root)
+}
---
+++ app/src/main/java/com/example/customgraph/HorizontalChartActivity.kt
@@ -0,0 +1,117 @@
+package com.example.customgraph
+
+import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TableLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.customgraph.databinding.ActivityHorizontalChartBinding
+
+class HorizontalChartActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHorizontalChartBinding
+    private lateinit var adapter: HorizontalBarChartAdapter
+    private lateinit var linearlayout: LinearLayout
+
+    private var granularity = 0
+    private var maxValue = 0
+    private var maxValueOnXAxis = 0
+    private var pointList = listOf(5, 8, 12, 18, 2, 3, 45, 5, 78, 65, 43, 66, 12)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHorizontalChartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViews()
+        getGranularity()
+        addTextViewForXAxis()
+        setGraphRecyclerViewAdapter(maxValueOnXAxis)
+    }
+
+    private fun initViews() {
+        linearlayout = binding.xAxisTextViewContainerLinearLayout
+    }
+
+    private fun getGranularity() {
+        maxValue = pointList.maxOrNull() ?: 0
+        calculateGranularity(maxValue)
+    }
+
+    //This method will calculate the granularity(interval) between two points of X-Axis.
+    private fun calculateGranularity(maxValue: Int) {
+        when {
+            maxValue > 10 -> {
+                maxValueOnXAxis = maxValue + (5 - (maxValue % 5))
+                granularity = maxValueOnXAxis / 5
+            }
+            maxValue > 8 -> {
+                maxValueOnXAxis = 10
+                granularity = 2
+            }
+            maxValue > 6 -> {
+                maxValueOnXAxis = 8
+                granularity = 2
+            }
+            maxValue > 4 -> {
+                maxValueOnXAxis = 6
+                granularity = 2
+            }
+            maxValue > 2 -> {
+                maxValueOnXAxis = 4
+                granularity = 2
+            }
+            maxValue > 1 -> {
+                maxValueOnXAxis = 2
+                granularity = 2
+            }
+            else -> {
+                maxValueOnXAxis = 1
+                granularity = 1
+            }
+        }
+    }
+
+    //This method will generate the required numbers of text-view for x-axis.
+    private fun addTextViewForXAxis() {
+        when {
+            maxValue > 10 -> createTextView(5)
+            maxValue > 8 -> createTextView(5)
+            maxValue > 6 -> createTextView(4)
+            maxValue > 4 -> createTextView(3)
+            maxValue > 2 -> createTextView(2)
+            else -> createTextView(1)
+        }
+    }
+
+    private fun createTextView(numberOfTextView: Int) {
+        generateXAxisOriginLabel()
+        for (index in 1..numberOfTextView) {
+            generateXAxisTextView((granularity * index).toString())
+        }
+    }
+
+    private fun generateXAxisOriginLabel() {
+        val textView = TextView(this)
+        textView.text = getString(R.string.zero)
+        linearlayout.addView(textView)
+    }
+
+    private fun generateXAxisTextView(text: String) {
+        val textView = TextView(this)
+        textView.text = text
+        textView.textAlignment = View.TEXT_ALIGNMENT_VIEW_END
+        textView.layoutParams = TableLayout.LayoutParams(
+            TableLayout.LayoutParams.WRAP_CONTENT,
+            TableLayout.LayoutParams.WRAP_CONTENT,
+            1f
+        )
+        linearlayout.addView(textView)
+    }
+
+    private fun setGraphRecyclerViewAdapter(maxValueOnXAxis: Int) {
+        adapter = HorizontalBarChartAdapter(pointList, maxValueOnXAxis)
+        binding.barChartRecyclerview.layoutManager = LinearLayoutManager(this)
+        binding.barChartRecyclerview.adapter = adapter
+    }
+}
